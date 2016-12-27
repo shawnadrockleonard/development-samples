781150b45bca4bfbb1c7aaacd99ecd461d3b7264
Shawn Leonard <shawnadrockleonard@gmail.com>
2016-12-27 08:44:40 -0700

    adding 04 sample for start of angular ts 2.1 example

---
+++ Sample04-SPAddIn/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="ba7b4f44-9a11-4ad3-8047-5867ab3e6f7e" description="SharePoint Add-in Feature" featureId="ba7b4f44-9a11-4ad3-8047-5867ab3e6f7e" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Sample04 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="dd385347-1fe6-4975-9abd-16d3e49721a0" />
+    <projectItemReference itemId="32a2ffee-45ed-496f-be3b-bb1a2c551abb" />
+    <projectItemReference itemId="7bb0de0e-29cc-4e60-99a1-228b0230f225" />
+    <projectItemReference itemId="992c4eaa-19a5-4df5-9a66-b8c55393cb6a" />
+  </projectItems>
+</feature>
---
+++ Sample04/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thisPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var pathcurrent = new System.IO.DirectoryInfo(thisPath);
+            var binFullPath = string.Format("{0}\\AppFiles", pathcurrent.Parent.Parent.Parent.FullName);
+
+            var pathContent = string.Format("{0}\\Content\\", binFullPath);
+            if (!System.IO.Directory.Exists(pathContent))
+            {
+                var outputPathDir = System.IO.Directory.CreateDirectory(pathContent);
+                System.Console.WriteLine(string.Format("Directory {0} created", outputPathDir.FullName));
+            }
+
+            var pathSiteAssets = string.Format("{0}\\SiteAssets", binFullPath);
+            if (!System.IO.Directory.Exists(pathSiteAssets))
+            {
+                var outputPathDir = System.IO.Directory.CreateDirectory(pathSiteAssets);
+                System.Console.WriteLine(string.Format("Directory {0} created", outputPathDir.FullName));
+            }
+
+            var model = new Sample04LibraryDefinition();
+            var jsonPath = string.Format("{0}\\Provisioner.json", pathContent);
+            var jsonProvisioner = JsonConvert.SerializeObject(model, Newtonsoft.Json.Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            System.IO.File.WriteAllText(jsonPath, jsonProvisioner);
+        }
+    }
+}
